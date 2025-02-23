e0a95817e687305ba7ceea2531abe3faae25a8dc
bnechlani09 <bnechlani09@gmail.com>
2025-02-23 22:39:17 +0530

    Checked in By: Bhavesh Nechlani Reason: 1) WalletTransactionType change

--- src/main/java/com/traders/portfolio/domain/WalletTransactionType.java
+++ src/main/java/com/traders/portfolio/domain/WalletTransactionType.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 @JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum WalletTransactionType {
     DEPOSIT,
-    WITHDRAWAL,
+    WITHDRAW,
     PROFIT,
     LOSS,
     SETTLEMENT,
